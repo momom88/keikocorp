176f339bd2e845c21606ec925df902b71395256a
Martin Zamarski <zamarskimartin@gmail.com>
2019-03-10 06:29:37 +0100

    Add files via upload

---
+++ precache-manifest.e329576d10fe4be3a96f9e0706f43658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9fa71fd449ffc1f9a70",
+    "url": "/facerecognitionbrain/static/js/runtime~main.a9fa71fd.js"
+  },
+  {
+    "revision": "7159885c046742794c44",
+    "url": "/facerecognitionbrain/static/js/main.7159885c.chunk.js"
+  },
+  {
+    "revision": "153adaaca761e453acfd",
+    "url": "/facerecognitionbrain/static/js/2.153adaac.chunk.js"
+  },
+  {
+    "revision": "7159885c046742794c44",
+    "url": "/facerecognitionbrain/static/css/main.049d69a7.chunk.css"
+  },
+  {
+    "revision": "153adaaca761e453acfd",
+    "url": "/facerecognitionbrain/static/css/2.8fac99e3.chunk.css"
+  },
+  {
+    "revision": "f05727df46b5148fb626b4241a2e7fcc",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
